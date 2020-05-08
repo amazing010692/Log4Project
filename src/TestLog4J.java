10f9701a8543d4013b23550854e7ac4cd784418f
Janielle Joy Gregorio <hello_janielle1992@yahoo.com.ph>
2020-05-09 00:34:05 +0800

    Apache Log4J API | Console and File Appenders

---
+++ src/TestLog4J.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class TestLog4J {
+	
+	static Logger log = Logger.getLogger(TestLog4J.class);
+	
+	public static void main(String[] args) {
+		
+		log.debug("This is a Debug Log");
+		log.info("This is an Info Log");
+		log.error("Error in a Project");
+
+	}
+
+}
